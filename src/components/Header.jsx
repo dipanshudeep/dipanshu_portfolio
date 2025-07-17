b91ef8a4a8a1663f2cefa2d20ee346c2773d7e66
Dipanshu <dipanshu200303@gmail.com>
2025-07-18 03:28:53 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,52 +8,51 @@ function Header() {
         className="w-2/4 flex items-center justify-evenly  px-4 sm:px-8 py-4 bg-white/30 backdrop-blur-sm shadow-2xl ring-2 ring-black/20 rounded-b-3xl sm:rounded-4xl"
         style={{ boxShadow: 'inset 2px 8px 15px white' }}
       >
-        {/* Nav links always visible */}
         <div className="flex flex-row space-x-2 sm:space-x-4">
           <NavLink
-            to='/'
+            to="/"
             className={({ isActive }) =>
-              block px-4 sm:px-6 py-2 sm:py-2.5 bg-black/0 rounded-2xl ring-2 ${
+              `block px-4 sm:px-6 py-2 sm:py-2.5 bg-black/0 rounded-2xl ring-2 ${
                 isActive
                   ? 'ring-amber-500 text-black font-bold'
                   : 'ring-black/0'
-              } hover:ring-amber-500 transition-all duration-300 text-sm sm:text-base
+              } hover:ring-amber-500 transition-all duration-300 text-sm sm:text-base`
             }
           >
             Home
           </NavLink>
           <NavLink
-            to='/projects'
+            to="/projects"
             className={({ isActive }) =>
-              block px-4 sm:px-6 py-2 sm:py-2.5 bg-black/0 rounded-2xl ring-2 ${
+              `block px-4 sm:px-6 py-2 sm:py-2.5 bg-black/0 rounded-2xl ring-2 ${
                 isActive
                   ? 'ring-amber-500 text-black font-bold'
                   : 'ring-black/0'
-              } hover:ring-amber-500 transition-all duration-300 text-sm sm:text-base
+              } hover:ring-amber-500 transition-all duration-300 text-sm sm:text-base`
             }
           >
             Projects
           </NavLink>
           <NavLink
-            to='/about'
+            to="/about"
             className={({ isActive }) =>
-              block px-4 sm:px-6 py-2 sm:py-2.5 bg-black/0 rounded-2xl ring-2 ${
+              `block px-4 sm:px-6 py-2 sm:py-2.5 bg-black/0 rounded-2xl ring-2 ${
                 isActive
                   ? 'ring-amber-500 text-black font-bold'
                   : 'ring-black/0'
-              } hover:ring-amber-500 transition-all duration-300 text-sm sm:text-base
+              } hover:ring-amber-500 transition-all duration-300 text-sm sm:text-base`
             }
           >
             About
           </NavLink>
           <NavLink
-            to='/contact'
+            to="/contact"
             className={({ isActive }) =>
-              block px-4 sm:px-6 py-2 sm:py-2.5 bg-black/0 rounded-2xl ring-2 ${
+              `block px-4 sm:px-6 py-2 sm:py-2.5 bg-black/0 rounded-2xl ring-2 ${
                 isActive
                   ? 'ring-amber-500 text-black font-bold'
                   : 'ring-black/0'
-              } hover:ring-amber-500 transition-all duration-300 text-sm sm:text-base
+              } hover:ring-amber-500 transition-all duration-300 text-sm sm:text-base`
             }
           >
             Contact
