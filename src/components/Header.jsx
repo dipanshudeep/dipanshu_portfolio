5d1d57a9895554142d13a3432ff457e774cbeac5
Dipanshu <dipanshu200303@gmail.com>
2025-07-18 03:12:02 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,16 @@ import { NavLink } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className="w-full fixed top-0 left-0 z-50 flex items-center justify-center mt-5 ">
+    <header className="w-full fixed top-0 left-0 z-50 flex items-center justify-center mt-5">
       <nav
-        className="w-2/4 flex items-center justify-evenly  px-4 sm:px-8 py-4 bg-white/30 backdrop-blur-sm shadow-2xl ring-2 ring-black/20 rounded-b-3xl sm:rounded-4xl"
+        className="w-full sm:w-full md:w-[85%] lg:w-2/4 flex items-center justify-center px-2 sm:px-4 md:px-6 py-3 bg-white/30 backdrop-blur-sm shadow-2xl ring-2 ring-black/20 rounded-b-2xl sm:rounded-4xl"
         style={{ boxShadow: 'inset 2px 8px 15px white' }}
       >
-        {/* Nav links always visible */}
-        <div className="flex flex-row space-x-2 sm:space-x-4">
+        <div className="flex flex-row flex-wrap justify-evenly gap-2 sm:gap-4">
           <NavLink
-            to='/'
+            to="/"
             className={({ isActive }) =>
-              `block px-4 sm:px-6 py-2 sm:py-2.5 bg-black/0 rounded-2xl ring-2 ${
+              `block px-4 sm:px-6 py-2 sm:py-2.5 bg-transparent rounded-2xl ring-2 ${
                 isActive
                   ? 'ring-amber-500 text-black font-bold'
                   : 'ring-black/0'
@@ -23,9 +22,9 @@ function Header() {
             Home
           </NavLink>
           <NavLink
-            to='/projects'
+            to="/projects"
             className={({ isActive }) =>
-              `block px-4 sm:px-6 py-2 sm:py-2.5 bg-black/0 rounded-2xl ring-2 ${
+              `block px-4 sm:px-6 py-2 sm:py-2.5 bg-transparent rounded-2xl ring-2 ${
                 isActive
                   ? 'ring-amber-500 text-black font-bold'
                   : 'ring-black/0'
@@ -35,9 +34,9 @@ function Header() {
             Projects
           </NavLink>
           <NavLink
-            to='/about'
+            to="/about"
             className={({ isActive }) =>
-              `block px-4 sm:px-6 py-2 sm:py-2.5 bg-black/0 rounded-2xl ring-2 ${
+              `block px-4 sm:px-6 py-2 sm:py-2.5 bg-transparent rounded-2xl ring-2 ${
                 isActive
                   ? 'ring-amber-500 text-black font-bold'
                   : 'ring-black/0'
@@ -47,9 +46,9 @@ function Header() {
             About
           </NavLink>
           <NavLink
-            to='/contact'
+            to="/contact"
             className={({ isActive }) =>
-              `block px-4 sm:px-6 py-2 sm:py-2.5 bg-black/0 rounded-2xl ring-2 ${
+              `block px-4 sm:px-6 py-2 sm:py-2.5 bg-transparent rounded-2xl ring-2 ${
                 isActive
                   ? 'ring-amber-500 text-black font-bold'
                   : 'ring-black/0'
